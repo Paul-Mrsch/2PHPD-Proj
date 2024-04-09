3c48cee05c5862b0c1723ee9257cb066c164a0c7
Paul Mareschi <117636519+Caalagan@users.noreply.github.com>
2024-04-09 10:05:33 +0200

    Added api controller for players

---
+++ src/Controller/ApiPlayerController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ApiPlayerController extends AbstractController
+{
+    #[Route('/api/players', name: 'api_players_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $em): JsonResponse
+    {
+        $players = $em->getRepository(User::class)->findAll();
+
+        return $this->json($players);
+    }
+
+    #[Route('/register', name: 'api_players_create', methods: ['POST'])]
+    public function create(EntityManagerInterface $em, SerializerInterface $serializerinterface, Request $request, ValidatorInterface $validator): JsonResponse
+    {
+        $player = $serializerinterface->deserialize($request->getContent(), User::class, 'json');
+
+        $errors = $validator->validate($player);
+
+        if (count($errors) > 0) {
+            $errorString = (string) $errors;
+            return new JsonResponse(['error' => $errorString], Response::HTTP_BAD_REQUEST);
+        }
+
+        $em->persist($player);
+        $em->flush();
+
+        return new JsonResponse(['message' => 'Player created!'], Response::HTTP_CREATED);
+    }
+
+    #[Route('/api/players/{id}', name: 'api_players_show', methods: ['GET'])]
+    public function show($id, EntityManagerInterface $em): JsonResponse
+    {
+        $player = $em->getRepository(User::class)->find($id);
+
+        if (!$player) {
+            return new JsonResponse(['error' => 'Player not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json($player);
+    }
+
+    #[Route('/api/players/{id}', name: 'api_players_edit', methods: ['PUT'])]
+    public function modify(EntityManagerInterface $em, SerializerInterface $serializerinterface, Request $request, ValidatorInterface $validator, $id)
+    {
+        $player = $em->getRepository(User::class)->find($id);
+
+        if (!$player) {
+            return new JsonResponse(['error' => 'Player not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $serializerinterface->deserialize($request->getContent(), User::class, 'json', ['object_to_populate' => $player]);
+
+        $errors = $validator->validate($player);
+
+        if (count($errors) > 0) {
+            $errorString = (string) $errors;
+            return new JsonResponse(['error' => $errorString], Response::HTTP_BAD_REQUEST);
+        }
+
+        $em->persist($player);
+        $em->flush();
+
+        return new JsonResponse(['message' => 'Player updated!'], Response::HTTP_OK);
+    }
+
+    #[Route('/api/players/{id}', name: 'api_players_delete', methods: ['DELETE'])]
+    public function delete(EntityManagerInterface $em, $id): JsonResponse
+    {
+        $player = $em->getRepository(User::class)->find($id);
+
+        if (!$player) {
+            return new JsonResponse(['error' => 'Player not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $em->remove($player);
+        $em->flush();
+
+        return new JsonResponse(['message' => 'Player deleted!'], Response::HTTP_OK);
+    }
+}
--- src/Controller/ApiTournamentsController.php
+++ src/Controller/ApiTournamentsController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Tournament;
-use Doctrine\Persistence\ObjectManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
